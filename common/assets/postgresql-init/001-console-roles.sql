a8d27d4e9b367359574e0398b5293df2db689c11
Philip Dubé <serprex@users.noreply.github.com>
2026-03-20 15:46:32 +0000

    Grant access to public schema for console roles (#18)

--- common/assets/postgresql-init/001-console-roles.sql
+++ common/assets/postgresql-init/001-console-roles.sql
@@ -5,6 +5,7 @@ CREATE USER chpg_console_admin WITH
     pg_read_all_stats, pg_read_all_settings, pg_checkpoint, pg_maintain,
     pg_create_subscription, pg_stat_scan_tables, pg_signal_autovacuum_worker;
 GRANT ALL ON DATABASE postgres TO chpg_console_admin;
+GRANT ALL ON SCHEMA public TO chpg_console_admin;
 ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO chpg_console_admin;
 ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCES TO chpg_console_admin;
 ALTER DEFAULT PRIVILEGES GRANT ALL ON FUNCTIONS TO chpg_console_admin;
@@ -13,3 +14,5 @@ ALTER DEFAULT PRIVILEGES GRANT ALL ON SCHEMAS TO chpg_console_admin;
 
 CREATE USER chpg_console_readonly;
 GRANT pg_read_all_data TO chpg_console_readonly;
+GRANT CONNECT ON DATABASE postgres TO chpg_console_readonly;
+GRANT USAGE ON SCHEMA public TO chpg_console_readonly;
