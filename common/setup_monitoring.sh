3aecea46cd4392db33cb9c71499e46b9816079a1
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2026-04-02 06:26:36 +0530

    refactor(otel): move base config to control-plane (#29)
    * refactor(otel): move base config to control-plane
    
    * chore: otel exporter should not error out

--- common/setup_monitoring.sh
+++ common/setup_monitoring.sh
@@ -60,6 +60,8 @@ mkdir -p /etc/systemd/system/otelcol-contrib.service.d
 cp /tmp/common/assets/otelcol-contrib-override.service /etc/systemd/system/otelcol-contrib.service.d/override.conf
 cp /tmp/common/otel-config.yaml /home/otelcol/otel-config.yaml
 chown otelcol:otelcol /home/otelcol/otel-config.yaml
+touch /home/otelcol/otel-config-override.yaml
+chown otelcol:otelcol /home/otelcol/otel-config-override.yaml
 touch /home/otelcol/otlp-export.token
 chown otelcol:otelcol /home/otelcol/otlp-export.token
 # Copy postgres_exporter queries
