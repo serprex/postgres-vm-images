d0b19d3b7802139ba8e5bd5044529d8efdb35fc8
Philip Dubé <159546+serprex@users.noreply.github.com>
2026-04-10 02:25:15 +0000

    Fix console role setup to use WITH ADMIN, not ADMIN (#33)
    https://www.postgresql.org/docs/current/sql-createrole.html
    
    > The ADMIN clause has the same effect as ROLE, but the named roles are added as members of the new role with ADMIN enabled, giving them the right to grant membership in the new role to others.
    
    Totally wrong thing, my bad. Also add WITH GRANT OPTION

--- common/assets/postgresql-init/001-console-roles.sql
+++ common/assets/postgresql-init/001-console-roles.sql
@@ -1,16 +1,15 @@
-CREATE USER chpg_console_admin WITH
-  CREATEDB
-  CREATEROLE
-  ADMIN pg_read_all_data, pg_write_all_data, pg_monitor, pg_signal_backend,
+CREATE USER chpg_console_admin WITH CREATEDB CREATEROLE;
+GRANT pg_read_all_data, pg_write_all_data, pg_monitor, pg_signal_backend,
     pg_read_all_stats, pg_read_all_settings, pg_checkpoint,
-    pg_create_subscription, pg_stat_scan_tables;
-GRANT ALL ON DATABASE postgres TO chpg_console_admin;
-GRANT ALL ON SCHEMA public TO chpg_console_admin;
-ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO chpg_console_admin;
-ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCES TO chpg_console_admin;
-ALTER DEFAULT PRIVILEGES GRANT ALL ON FUNCTIONS TO chpg_console_admin;
-ALTER DEFAULT PRIVILEGES GRANT ALL ON TYPES TO chpg_console_admin;
-ALTER DEFAULT PRIVILEGES GRANT ALL ON SCHEMAS TO chpg_console_admin;
+    pg_create_subscription, pg_stat_scan_tables
+  TO chpg_console_admin WITH ADMIN OPTION;
+GRANT ALL ON DATABASE postgres TO chpg_console_admin WITH GRANT OPTION;
+GRANT ALL ON SCHEMA public TO chpg_console_admin WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO chpg_console_admin WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCES TO chpg_console_admin WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES GRANT ALL ON FUNCTIONS TO chpg_console_admin WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES GRANT ALL ON TYPES TO chpg_console_admin WITH GRANT OPTION;
+ALTER DEFAULT PRIVILEGES GRANT ALL ON SCHEMAS TO chpg_console_admin WITH GRANT OPTION;
 
 CREATE USER chpg_console_readonly;
 GRANT pg_read_all_data TO chpg_console_readonly;
